7756e566f97a60d305f860a54c2724ae94f80f28
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2024-01-10 07:49:06 +0200

    start work on Players section when there are no players added yet

--- components/PlayersSection.tsx
+++ components/PlayersSection.tsx
@@ -1,6 +1,7 @@
 import { Player, skillTierNames } from '@/types/Player';
 import React from 'react';
 import PlayerChip from './PlayerChip';
+import AddPlayerDialog from './AddPlayerDialog';
 
 interface PlayersComponentProps {
   numOfTiers: number;
@@ -9,14 +10,23 @@ interface PlayersComponentProps {
 }
 /**Players Section of app */
 export default function PlayersSection(props: PlayersComponentProps) {
-  const { numOfTiers, registeredPlayers, onClickDelete } = props; 
+  const { numOfTiers, registeredPlayers, onClickDelete } = props;
+
+  const PlayerView = () => {
+    return registeredPlayers.length < 1 
+      ? <p>No players added.</p>
+      : (<div className='border-solid border-2 border-orange-300 rounded-lg p-5 m-5'>
+        <div>{registeredPlayers.map(
+          (p) => <PlayerChip key={`${p[0]}${p[1]}`} player={p} onClickDelete={onClickDelete}/>)}
+        </div>
+        <div className=''> <p className='text-right' >Registered players: {registeredPlayers.length}</p> </div>
+      </div>);
+  };
+
   return (
-    <div>
-      <div>{registeredPlayers.map(
-        (p) => <PlayerChip key={`${p[0]}${p[1]}`} player={p} onClickDelete={onClickDelete}/>)}
-        
-      </div>
-      <div> <p>Registered players {registeredPlayers.length}</p> </div>
+    <div className='p-1'>
+      {PlayerView()}
+      <AddPlayerDialog />
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,25 @@ import PlayersSection from '@/components/PlayersSection';
 import { Player } from '@/types/Player';
 import { useState } from 'react';
 
+const testPlayers: Player[] = [['Pasi', 4], ['Hung', 1],  ['Hung', 2], ['Jack', 2], ['Sammy', 3], ['Yohan', 3], ['Kofi', 3], ['Artem', 3], ['Gylfi', 2],   ['Kwame', 4], ['Wahid', 1], ['Paul', 3], ['Ilya', 1]];
+
 
 export default function Home() {
   const [numOfTeams, setNumOfTeams] = useState<number>(2);
   const [numOfTiers, setNumOfTiers] = useState<number>(3);
-  const [registeredPlayers, setRegisteredPlayers] = useState<Array<Player>>([['Pasi', 4], ['Kwame', 4], ['Sammy', 3], ['Paul', 3], ['Yohan', 3], ['Kofi', 3], ['Artem', 3], ['Gylfi', 2], ['Hung', 2], ['Jack', 2], ['Wahid', 1], ['Ilya', 1]]);
+  const [registeredPlayers, setRegisteredPlayers] = useState<Array<Player>>([]);
   
   const handlePlayerDelete = (playerToBeDeleted: Player) => {
     console.log('playerToBeDeleted ', playerToBeDeleted);
-    setRegisteredPlayers(registeredPlayers.filter((p) => p[0]!== playerToBeDeleted[0]/*filter by name */ ));
+
+    // setRegisteredPlayers(registeredPlayers.filter((p) => p[0]!== playerToBeDeleted[0]/*filter by name */ ));
+    setRegisteredPlayers(registeredPlayers.filter(
+      (p) => !(p[0] === playerToBeDeleted[0] && p[1] === playerToBeDeleted[1])
+    ));
   };
 
   return (
-    <>
+    <div className='p-5'>
       <GameConfigSection 
         numOfTeams={numOfTeams}
         numOfTiers={numOfTiers}
@@ -27,9 +33,6 @@ export default function Home() {
         registeredPlayers={registeredPlayers}
         onClickDelete={handlePlayerDelete}
       /> 
-    </>
+    </div>
   );
 }
-
-
-
